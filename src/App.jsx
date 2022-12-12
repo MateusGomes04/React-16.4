591fb141884d76c415b7b366e994b82f04ea8afe
MateusGomes04 <mateusgomesc04@gmail.com>
2022-12-12 12:14:36 -0300

    first component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Primeiro from "./components/primeiro";
+
+function App() {
+  return (
+    <div className="App">
+      <Primeiro></Primeiro>
+    </div>
+  );
+}
+
+export default App;
