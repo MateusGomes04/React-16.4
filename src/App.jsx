9c096179441c44f150f24ce5fa5907b7275bfbbe
MateusGomes04 <mateusgomesc04@gmail.com>
2022-12-12 12:31:25 -0300

    multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Primeiro from "./components/primeiro";
+import Morning from "./components/bomdia";
+import { BoaNoite, BoaTarde } from "./components/multi";
 
 function App() {
   return (
     <div className="App">
       <Primeiro></Primeiro>
+      <Morning name="mateus"></Morning>
+      <BoaTarde name="Bia"></BoaTarde>
+      <BoaNoite name="Lucas"></BoaNoite>
     </div>
   );
 }
---
+++ src/components/bomdia.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Morning(props) {
+  return (
+    <div>
+      <h1>Bom dia {props.name}</h1>
+      <h2>Até breve!</h2>
+    </div>
+  );
+}
